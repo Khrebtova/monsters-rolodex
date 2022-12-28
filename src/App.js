1f7e5900ece94211cf184fbe7966e081233e798f
Kseniia McFadden <mcfaddenkseniia@gmail.com>
2022-12-28 15:18:02 -0800

    create basic file structure

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
+import { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      monsters : []
+    }  
+  }  
+
+  componentDidMount = () => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => this.setState({monsters: users}))
+    .catch(error => console.log(error))
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1 className='app-title '>Monsters Rolodex</h1>
+        {this.state.monsters.map(monster => {
+          return (
+            <div key={monster.id}>
+              <h2>{monster.name} - {monster.username}</h2>
+            </div>
+          )
+        })
+        }
+      </div>
+    );
+  }
 }
 
 export default App;
