a72262c3fe00113ca77dae8302b6ed04cd6bdfc5
Kseniia McFadden <mcfaddenkseniia@gmail.com>
2022-12-28 16:40:24 -0800

    add filter function

--- src/App.js
+++ src/App.js
@@ -5,22 +5,37 @@ class App extends Component {
   constructor(){
     super();
     this.state = {
-      monsters : []
+      monsters : [],
+      searchField: ''
     }  
   }  
-
+  
   componentDidMount = () => {
     fetch('https://jsonplaceholder.typicode.com/users')
     .then(response => response.json())
-    .then(users => this.setState({monsters: users}))
+    .then(users => this.setState({monsters: users, displayMonsters: users}))
     .catch(error => console.log(error))
-  }
+  } 
 
+  handleSearch = (e) => {
+    this.setState({searchField: `${e.target.value}`.toLowerCase()})
+  }  
+  
   render() {
+    const { monsters, searchField } = this.state;
+    const { handleSearch } = this;
+    const filteredMonsters = monsters.filter(monster => monster.name.toLowerCase().includes(searchField))
+
     return (
       <div className="App">
         <h1 className='app-title '>Monsters Rolodex</h1>
-        {this.state.monsters.map(monster => {
+        <input 
+          type='search' 
+          placeholder='search monsters' 
+          className='search-box' 
+          onChange={handleSearch}
+        />
+        {filteredMonsters.map(monster => {
           return (
             <div key={monster.id}>
               <h2>{monster.name} - {monster.username}</h2>
---
+++ src/components/search-box/search-box.component.jsx
@@ -0,0 +1,16 @@
+import React, { Component} from 'react'
+
+class SearchBox extends Component {
+    constructor(props){
+
+    }
+
+    render(){
+        
+      return (
+      <div>search-box.component</div>
+    )
+  }
+}
+
+export default SearchBox
